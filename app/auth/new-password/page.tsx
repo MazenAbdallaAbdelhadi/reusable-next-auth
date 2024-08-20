36511ddd3f09010223b9b1eaa8bc748109102d33
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2024-08-20 05:00:26 +0300

    add suspense
    add suspense to prevent the whole page from being client pages

--- app/auth/new-password/page.tsx
+++ app/auth/new-password/page.tsx
@@ -1,7 +1,12 @@
 import { NewPasswordForm } from "@/components/auth/new-reset-form";
+import { Suspense } from "react";
 
 const NewPasswordPage = () => {
-  return <NewPasswordForm/>;
+  return (
+    <Suspense>
+      <NewPasswordForm />
+    </Suspense>
+  );
 };
 
 export default NewPasswordPage;
--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -1,7 +1,12 @@
 import { ResetForm } from "@/components/auth/reset-form";
+import { Suspense } from "react";
 
 const ResetPage = () => {
-  return <ResetForm/>;
+  return (
+    <Suspense>
+      <ResetForm />
+    </Suspense>
+  );
 };
 
 export default ResetPage;
